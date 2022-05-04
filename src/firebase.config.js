0b3e5fd8bd4ef40269274801e6525cde6dab7472
David Vekony <68352412+davidvekony@users.noreply.github.com>
2022-05-04 09:25:01 +0100

    initial firebase, styling, page, and navbar setup

---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBOj3_g638PQbzTXzrOl_dKrL_lBZvZiwE",
+  authDomain: "house-marketplace-app-b8eaa.firebaseapp.com",
+  projectId: "house-marketplace-app-b8eaa",
+  storageBucket: "house-marketplace-app-b8eaa.appspot.com",
+  messagingSenderId: "1038311025360",
+  appId: "1:1038311025360:web:24677dc75b975113e1ccd5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
