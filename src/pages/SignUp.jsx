0f6004509483c492aeae80c1d00375fe665b8a79
David Vekony <68352412+davidvekony@users.noreply.github.com>
2022-05-04 10:16:17 +0100

    implement Firebase authentication

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { db } from "../firebase.config";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
@@ -21,13 +27,36 @@ function SignUp() {
     }));
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    
+    try {
+      const auth = getAuth();
+      
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      
+      const user = userCredential.user;
+      
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      });
+      
+      navigate("/");
+
+    } catch (error) {console.log(error)}
+  };
+
   return (
     <>
       <div className="pageContainer">
         <header>
           <p className="pageHeader">Welcome Back!</p>
         </header>
-        <form>
+        <form onSubmit={onSubmit}>
           <input
             type="text"
             className="nameInput"
@@ -64,7 +93,7 @@ function SignUp() {
             Forgot Password?
           </Link>
           <div className="signUpBar">
-            <p className="signUpText">Sign In</p>
+            <p className="signUpText">Sign Up</p>
             <button className="signUpButton">
               <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
             </button>
